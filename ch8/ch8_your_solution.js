9b1ac1d66f15d197b14414a2b4227ad03b24fa9d
Bryan Bierce <bryanbierce@gmail.com>
2016-03-31 09:55:23 -0700

    Added tests for 16-6

--- ch8/ch8_your_solution.js
+++ ch8/ch8_your_solution.js
@@ -1,12 +1,1 @@
-function recursiveMultiply(a, b, sum) {
-  const multiplier = a <= b ? a : b;
-  const multiplicand = a >= b ? a : b;
-  sum = sum || 0;
-
-  if(multiplier === 0) {
-    return sum;
-  } else {
-    sum += multiplicand
-    return recursiveMultiply(multiplier-1, multiplicand, sum);
-  }
-}
+function recursiveMultiply(a, b, sum) {}
