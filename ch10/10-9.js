4f301dc5e13ffb7a8aff616610d92f242b7227f0
Bryan Bierce <bryanbierce@gmail.com>
2016-04-08 22:12:17 -0700

    Solved 10-9

--- ch10/10-9.js
+++ ch10/10-9.js
@@ -2,3 +2,45 @@
 
 
 /*                  Individual Solutions                    */
+
+
+//       Bryan
+
+function sortedMatrixSearch(matrix, target) {
+  const result = [];
+  let container;
+  let possible = matrix.map((v, i) => i);
+
+  while (possible.length && container === undefined) {
+    let pMid = Math.floor(possible.length / 2);
+    let midIndex = possible[pMid];
+    let midRow = matrix[midIndex];
+
+    if (target >= midRow[0] && target <= midRow[midRow.length-1]) {
+      container = midRow;
+      result.push(midIndex);
+    } else if (target > midRow[midRow.length-1]) {
+      possible = possible.length > 1 ? possible.slice(pMid+1) : [];
+    } else {
+      possible = possible.slice(0, pMid);
+    }
+  }
+
+  possible = container ? container.map((v, i) => i) : [];
+
+  while (possible.length && result.length !== 2) {
+    let pMid = Math.floor(possible.length / 2);
+    let midIndex = possible[pMid];
+    let midVal = container[midIndex];
+
+    if(midVal === target) {
+      result.push(midIndex);
+    } else  if (target > midVal) {
+      possible = possible.length > 1 ? possible.slice(pMid+1) : [];
+    } else {
+      possible = possible.slice(0, pMid);
+    }
+  }
+
+  return result;
+}
