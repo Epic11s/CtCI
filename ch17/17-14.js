70a83ca1ea3b2f114eaf5733df214b3e2db17dce
Bryan Bierce <bryanbierce@gmail.com>
2016-04-20 10:10:59 -0700

    Added solution to 17-14

--- ch17/17-14.js
+++ ch17/17-14.js
@@ -2,3 +2,26 @@
 
 
 /*                  Individual Solutions                    */
+
+
+//          Bryan
+function smallestK(array, targetNum) {
+  return array.reduce(function(result, val) {
+    if (result.length < targetNum) {
+      result.push(val);
+    } else {
+      for (var i = result.length-1; i >= 0; i--) {
+        if (val <= result[i]) {
+          if ((i > 0 && val >= result[i-1]) || i === 0) {
+            result.splice(i,0,val);
+            result.pop();
+            break;
+          } else if (val > result[i]) {
+            break;
+          }
+        }
+      }
+    }
+    return result;
+  }, []);
+}
